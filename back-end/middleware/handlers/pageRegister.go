c088f0bb989f2cfba9b37b7f2052ea403d251fdf
aauvray <auvrayarman@gmail.com>
2024-04-15 12:43:07 +0200

    start back arman branch

--- back-end/middleware/handlers/pageRegister.go
+++ back-end/middleware/handlers/pageRegister.go
@@ -1,5 +1,22 @@
 package handlers
 
-func handlerRegister() {
-	
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func handlerRegister(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("../social-network/front-end/htmls/page_register.html")
+	if err != nil {
+		fmt.Println("Error loading template:", err)
+		http.Error(w, "Erreur lors du chargement du modèle", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, r)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		http.Error(w, "Erreur lors de l'exécution du modèle", http.StatusInternalServerError)
+		return
+	}
 }
