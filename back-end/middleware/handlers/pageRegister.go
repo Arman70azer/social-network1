7817bb7660097734de4b71b2558069c35e92e204
aauvray <auvrayarman@gmail.com>
2024-04-15 14:34:53 +0200

    execute

--- back-end/middleware/handlers/pageRegister.go
+++ back-end/middleware/handlers/pageRegister.go
@@ -2,10 +2,17 @@ package handlers
 
 import (
 	"back-end/structures"
+	"fmt"
 	"net/http"
 )
 
 func HandlerRegister(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		fmt.Println("Le formulaire a été soumis avec succès")
+		email := r.FormValue("email")
+		fmt.Println("Voici l'adresse e-mail fournie : ", email)
+	}
 
 	var data structures.Data
 
