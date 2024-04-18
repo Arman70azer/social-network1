8bd1dee3db18e35404d02cdd55de5abf4094baa0
toufik@Toufik <toufik@Toufik>
2024-04-18 15:13:03 +0200

    social

---
+++ front-end/src/app/ui/button.jsx
@@ -0,0 +1,14 @@
+export default function Button() {
+    return (
+    <>
+        <h1>
+            test button
+        </h1>
+        <button>
+            click me 
+        
+        </button>
+    </>
+    
+    )
+}
