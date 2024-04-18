c2990336ddaa48eb0bccd530d8a225441b6b36b6
aauvray <auvrayarman@gmail.com>
2024-04-18 11:46:06 +0200

    h

---
+++ front-end/src/app/components/dashboard.jsx
@@ -0,0 +1,15 @@
+import styles from '../styles/home.module.css'; // Utilisez des guillemets simples ou doubles pour l'importation
+
+function DashboardTop () {
+    return (
+        <div className={styles.dashboardTopPage}>
+            <div className={styles.titleHome}>Social-Network</div>
+            <button className={styles.buttonConversations}>Conversations</button>
+            <button className={styles.buttonNotif}>Notifications</button>
+            <button className={styles.buttonProfil}>Profil</button>
+            <button className={styles.buttonLogout}>Logout</button>
+        </div>
+    );
+}
+
+export default DashboardTop;
--- front-end/src/app/home/page.jsx
+++ front-end/src/app/home/page.jsx
@@ -1,13 +1,7 @@
-import styles from '../styles/home.module.css'; // Utilisez des guillemets simples ou doubles pour l'importation
+import DashboardTop from "../components/dashboard"
 
 export default function Page() {
     return (
-        <div className={styles.dashboardTopPage}>
-            <div className={styles.titleHome}>Social-Network</div>
-            <button className={styles.buttonConversations}>Conversations</button>
-            <button className={styles.buttonNotif}>Notifications</button>
-            <button className={styles.buttonProfil}>Profil</button>
-            <button className={styles.buttonLogout}>Logout</button>
-        </div>
-    );
+    <DashboardTop/>
+    )
 }
