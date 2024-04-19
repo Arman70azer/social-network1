715cf6035dea8a0dace1c67d100ca7bcfb4c3057
aauvray <auvrayarman@gmail.com>
2024-04-19 13:59:11 +0200

    update style post

--- front-end/src/app/home/page.jsx
+++ front-end/src/app/home/page.jsx
@@ -14,23 +14,35 @@ export default async function Page(){
             <div className={styles.Content}>     
                 {posts.map((post, index) => (
                     <div key={index} className={styles.windowPost} id={`postBy${post.Author}`}>
-                        <div className={styles.avatarProfil}>
-                            <img className={styles.avatarPost} src={`/images/${post.Author.ImageName}`}/>
+                        <div className={styles.alineProfilPost}>
+                            <div className={styles.avatarProfil}>
+                                <img className={styles.avatarPost} src={`/images/${post.Author.ImageName}`}/>
+                            </div>
+                            <button className={styles.authorPost}>
+                                {post.Author.Nickname}
+                            </button>
                         </div>
-                        <div className={styles.authorPost}>
-                            --- {post.Author.Nickname} ---
+                        <div className={styles.postContent}>
+                            <div className={styles.titlePost}>
+                                {post.Titre}:
+                            </div>
+                            <div className={styles.imagePostContainer}>
+                                <img className={styles.imagePost} src={`/images/${post.Image}`}/>
+                            </div>
+                            <div className={styles.contentPost}>
+                                {post.Content}
+                            </div>
                         </div>
-                        <div className={styles.titlePost}>
-                            {post.Titre}:
-                        </div>
-                        <div className={styles.imagePostContainer}>
-                            <img className={styles.imagePost} src={`/images/${post.Image}`}/>
-                        </div>
-                        <div className={styles.contentPost}>
-                            {post.Content}
-                        </div>
-                        <div className={styles.datePost}>
-                            {post.Date}
+                        <div className={styles.postFooter}>
+                            <div className={styles.buttonLike}>
+                                <button>Like</button>
+                            </div>
+                            <div className={styles.buttonDislike}>
+                                <button className={styles.marginLeft}>Dislike</button>
+                            </div>
+                            <div className={styles.datePost}>
+                                {post.Date}
+                            </div>
                         </div>
                     </div>
                 ))}
